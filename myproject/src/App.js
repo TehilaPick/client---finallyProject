0dca6d17b8505928a98bd19daabf7f13741b3a54
Esther Winkler <5710490@gmail.comgit config --global user.name>
2022-01-03 22:37:13 +0200

    commit

--- myproject/src/App.js
+++ myproject/src/App.js
@@ -17,6 +17,7 @@ import {
 } from "react-router-dom";
 import Register from './components/register';
 import Login from './components/login';
+import Home from './components/home';
 
 
 function App() {
@@ -45,18 +46,20 @@ function App() {
      <Navbar.Brand >ארועים</Navbar.Brand>
      <Navbar.Toggle aria-controls="basic-navbar-nav" />
      <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
+        <Nav className="me-auto">
         <Nav.Link >
         <Link to="/register">הרשמה</Link>
         </Nav.Link>
         <Nav.Link >
         <Link to="/login">התחברות</Link>
         </Nav.Link>
-        
-      </Nav>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
+        <Nav.Link >
+        <Link to="/home">דף הבית</Link>
+        </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+   </Navbar>
         {/* <nav>
           <ul> */}
             {/* <li>
@@ -80,6 +83,9 @@ function App() {
           <Route path="/login">
             <Login />
           </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
           {/* <Route path="/">
             <App />
           </Route> */}
@@ -88,14 +94,14 @@ function App() {
     </Router>
 
  
-
+{/* 
        <InputGroup  className="p-3 SearchBox">
        <InputGroup.Text dir="rtl" id="inputGroup-sizing-default">חיפוש</InputGroup.Text>
        <FormControl 
          aria-label="Default"
          aria-describedby="inputGroup-sizing-default"
        />
-       </InputGroup>
+       </InputGroup> */}
     </div>
   );
 }
--- myproject/src/components/register.js
+++ myproject/src/components/register.js
@@ -1,8 +1,103 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import FormControl from 'react-bootstrap/FormControl';
+import InputGroup from 'react-bootstrap/InputGroup';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import './register.css';
+import './login.css';
+import axios from 'axios';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect
+  } from "react-router-dom";
 
 export default function Register(){
-    
-    return(
-        <>Register</>
+    const [email, setEmail]= useState("");
+    const [name, setName]= useState("");
+    const [password,setPassword] = useState("");
+    const [phone,setPhone] = useState("");
+    const [isRegister, setIsRegister]=useState(0);
+
+
+    useEffect(()=>{
+       setIsRegister(0);
+    },[])
+
+    const handleEmailChange= (eve) =>{
+        setEmail(eve.target.value);
+        console.log(email)
+    }
+
+    const handleNameChange =(eve)=>{
+        console.log(name);
+        setName(eve.target.value);
+        
+    }
+
+    const handlePasswordChange =(eve)=>{
+        setPassword(eve.target.value)
+        console.log(password)
+    }
+
+    const handlePhoneChange=(eve)=>{
+        setPhone(eve.target.value)
+    }
+
+    const submit=(event)=>{
+       
+        axios.post('http://localhost:3001/createNewContact',{
+            contactname:  name,
+            email: email,
+            phone: phone,
+            permission: false
+        })
+        .then(res=>{
+            //console.log(res);
+            if(res.status == 200){
+               setIsRegister(1)
+            }
+        })
+
+        setEmail(""); setName(""); setPhone("");
+        event.preventDefault();
+
+    }
+
+
+    if(isRegister==0){
+      return(
+       <Form className="formRegister" onSubmit={submit}>
+          
+         <Form.Group className="mb-3" controlId="formBasicName">
+         <Form.Control type="text" placeholder="שם משתמש"
+          value={email} onChange={handleEmailChange}  />
+         </Form.Group>
+      
+         <Form.Group className="mb-3" controlId="formBasicEmail">
+         <Form.Control type="email" placeholder="אימייל" 
+          value={name} onChange={handleNameChange}/>
+         </Form.Group>
+      
+         <Form.Group className="mb-3" controlId="formBasicPhone">
+         <Form.Control type="text" placeholder="טלפון" 
+          value={phone} onChange={handlePhoneChange} />
+         </Form.Group>
+      
+         <Button variant="primary" type="submit" >
+          להרשמה
+         </Button>
+      </Form>
+      )
+    }
+    if(isRegister ==1){
+        return(
+         <><div className="loginInfo"><Button variant="outline-danger" onClick={()=>{setIsRegister(2);  }} className="x">X</Button>נרשמת בהצלחה</div> </> 
+         )
+    }
+    else return(
+        <> <Redirect to="/home" /></>
     )
 }
